466c755f2f24bacb958ff7852ba31eb88d289cd8
badjokeresult <badjokeresult@gmail.com>
2023-11-01 01:23:56 +0300

    refactoring-and-debug-upds

--- 4_test.py
+++ 4_test.py
@@ -4,84 +4,65 @@ from elasticsearch7 import Elasticsearch
 
 import psycopg2
 import requests
+import warnings
 
 
 def check_nodejs(url):
 	try:
 		response = requests.get(url) # Выполняем GET-запрос к приложению
 		response.raise_for_status() # Если код статуса равен 4xx или 5xx, то вызываем исключение
-	except requests.exceptions.HTTPError:
-		print("[-] NodeJS works, but returns a bad status code") # Перехватываем исключение о ошибочном статусе и возвращаем False
-		return False
-	except requests.exceptions.ConnectionError: # Перехватываем ошибку невозможности подключения и возвращаем False
-		print("[-] Unable to connect to NodeJS")
-		return False
-	except Exception as e: # Перехватываем все остальные исключения и снова вызываем его
-		print("[-] Something went wrong during perfomance check")
-		raise e
-	else: # Если не было исключений, то пишем об успешности и возвращаем True
-		print("[*] NodeJS perfomance check is successful")
+		print(response.text) # Пишем текст ответа от сервера, чтобы в случае ошибки понять, в чем может быть проблема
 		return True
+	except Exception as e: # Перехватываем все исключения и снова вызываем его
+		print(e) # Пишем текст исключения, чтобы понять, где произошла ошибка
+		return False
 
 
 def check_postgres(**kwargs):
 	try:
+		response = None
 		# Подключаемся к БД
 		conn = psycopg2.connect(database=kwargs["database"], 
         	                	host=kwargs["host"],
             	            	user=kwargs["user"],
                 	        	password=kwargs["password"],
                     	    	port=kwargs["port"])
-		cursor = conn.cursor() # Создаем объект курсора
-		cursor.execute("SELECT * FROM pg_catalog.pg_user") # Пробуем выполнить простой запрос
-	except psycopg2.OperationalError: # Перехватываем ошибку подключения
-		print("[-] Unable to connect to PostgreSQL")
+		with conn.cursor() as cursor: # Создаем объект курсора
+			cursor.execute("SELECT * FROM pg_catalog.pg_user") # Пробуем выполнить простой запрос
+			response = cursor.fetchmany(10) # Получаем первые 10 записей из запроса
+		print(response) # Пишем первые 10 записей или что-либо другое, что возвращает БД (для упрощения багфикса)
+		return True
+	except Exception as e: # Перехватываем все исключения
+		print(e) # Пишем текст исключения для упрощения багфикса
 		return False
-	except Exception as e: # Перехватываем прочие ошибки
-		print("[-] Something went wrong during perfomance check")
-		raise e
-	else:
-		print("[*] PostgreSQL perfomance check is successful")
-		return True
 
 
-def check_elastic(url):
-	# Подключаемся к Elasticsearch
-	es = Elasticsearch(
-		hosts=[url],
-		basic_auth=("elastic", "testpassword"),
-	)
-
+def check_elastic(**kwargs):
+	user, password, host, port = kwargs["user"], kwargs["password"], kwargs["host"], kwargs["port"]
 	try:
-		if not es.cluster.health(): # Пытаемся узнать состояние кластера
-			print("[-] Unable to connect to Elasticsearch")
-			return False
-	except Exception as e: 
-		print("[-] Something went wrong during perfomance check")
-		raise e
-	else:
-		print("[*] Elasticsearch perfomance check is successful")
+		es = Elasticsearch(f"http://{user}:{password}@{host}:{port}") # Подключаемся к elasticsearch
+		info = None
+		with warnings.catch_warnings(record=True) as warns: # Перехватываем предупреждения и пишем их
+			info = es.info() # Пробуем получить информацию о системе
+		print(info) # Пишем инфу о системе или что-то другое, что она отдала
 		return True
+	except Exception as e: # Перехватываем исключения
+		print(e) # Пишем перехваченные исключения
+		return False
 
 
 def main():
 	# Проверяем NodeJS
-	if check_nodejs("http://localhost:3000"):
-		print("[*] NodeJS check passed\n")
-	else:
-		print("[-] NodeJS check failed\n")
+	(sign, result) = ("*", "passed") if check_nodejs("http://localhost:3000") else ("-", "failed")
+	print(f"[{sign}] NodeJS check {result}\n")
 
 	# Проверяем PostgreSQL
-	if check_postgres(database="testdb", host="localhost", user="testuser", password="testpassword", port="5432"):
-		print("[*] PostgreSQL check passed\n")
-	else:
-		print("[-] PostgreSQL check failed\n")
+	(sign, result) = ("*", "passed") if check_postgres(database="testdb", host="localhost", user="testuser", password="testpassword", port="5432") else ("-", "failed")
+	print(f"[{sign}] PostgreSQL check {result}\n")
 
 	# Проверяем Elasticsearch
-	if check_elastic("http://localhost:9200"):
-		print("[*] Elasticsearch check passed\n")
-	else:
-		print("[-] Elasticsearch check failed\n")
+	(sign, result) = ("*", "passed") if check_elastic(user="elastic", host="localhost", password="testpassword", port="9200") else ("-", "failed")
+	print(f"[{sign}] Elasticsearch check {result}\n")
 
 
 if __name__ == "__main__":
